a0568a7e27248d8f7bc16139523590f794bab7de
yechentide <yechentide@gmail.com>
2021-01-02 12:26:43 +0900

    Defined nbt tag type & error type

---
+++ Sources/NBTEditor/Enums.swift
@@ -0,0 +1,19 @@
+public enum NBTagType: Int {
+    case End
+    case Byte
+    case Short
+    case Int
+    case Long
+    case Float
+    case Double
+    case ByteArray
+    case String
+    case List
+    case Compound
+    case IntArray
+    case LongArray
+}
+
+public enum NBTError: Error {
+    case wrongPayloadLength
+}
